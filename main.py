e97e95315ba32c69d6c81763f4c8b6359f732bfd
Abelardo0503 <87998594+Abelardo0503@users.noreply.github.com>
2023-09-02 13:52:59 -0500

    Las bases

---
+++ main.py
@@ -0,0 +1,50 @@
+import discord
+import os
+from discord.ext import commands
+import requests
+import asyncio
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(
+    command_prefix="!",  # Change to desired prefix
+    case_insensitive=True  # Commands aren't case-sensitive
+)
+
+@bot.event 
+async def on_ready():  # When the bot is ready
+    print("Bot iniciado")
+    print(bot.user)  # Prints the bot's username and identifier
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="desarrollo"))
+
+    # Configura el intervalo de tiempo en segundos para hacer la petición
+    intervalo = 270  # 5 minutos (puedes ajustar este valor)
+
+    while True:
+        # Realiza una petición a tu bot para mantenerlo activo
+        url = f"https://replit.com/{os.environ['REPL_SLUG']}.{os.environ['REPL_OWNER']}"
+        response = requests.get(url)
+        print("Ping enviado para mantener el bot activo")
+        await asyncio.sleep(intervalo)
+
+extensions = [
+    # 'cogs.cogs_basicos',
+    'cogs.wiki',
+    'cogs.industria',
+    'cogs.ayuda',
+    'cogs.economía'
+]
+
+if __name__ == '__main__':
+    for extension in extensions:
+        bot.load_extension(extension)
+
+
+    try:
+        bot.run(os.getenv("TOKEN"))
+    except discord.HTTPException as e:
+        # Manejo de errores
+        pass  # Agrega aquí tu manejo de errores específico si es necesario
+
